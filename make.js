00214488c6f7b2369ba08507b7e2a329029a510b
Volodymyr Bobko <volodymyr-bobko@users.noreply.github.com>
2024-04-10 13:07:05 +0200

    SIGN-6728 - added readme and action to the build result

--- make.js
+++ make.js
@@ -19,6 +19,9 @@ CLI.build = function(args) {
     run('tsc --rootDir actions');
     run('ncc build index.js -o dist', true, 'actions/submit-signing-request');
 
+    fs.copyFileSync('README.md', 'actions/submit-signing-request/dist/README.md');
+    fs.copyFileSync('actions/submit-signing-request/action.yml', 'actions/submit-signing-request/dist/action.yml');
+    
     run('npm run test', true);
 }
 
