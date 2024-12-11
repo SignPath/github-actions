173fcbca114bad19fb18889a6b70e06ab731b197
Volodymyr Bobko <volodymyrbobko@gmail.com>
2024-12-11 16:03:54 +0100

    SIGN-7471 - connector logs support

--- actions/submit-signing-request/dtos/submit-signing-request-result.ts
+++ actions/submit-signing-request/dtos/submit-signing-request-result.ts
@@ -3,6 +3,7 @@ export interface SubmitSigningRequestResult {
     validationResult: ValidationResult;
     signingRequestUrl: string;
     error: string;
+    logs: LogEntry[];
 }
 
 export interface ValidationResult {
@@ -13,3 +14,13 @@ export interface ValidationError {
     error: string;
     howToFix: string;
 }
+
+export interface LogEntry {
+    message: string;
+    level: string;
+}
+
+export const LogLevelDebug = "Debug";
+export const LogLevelInformation = "Information";
+export const LogLevelWarning = "Warning";
+export const LogLevelError = "Error";
